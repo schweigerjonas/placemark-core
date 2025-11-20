0c8cc4b9fe38b04a4424f58a60e396de34592461
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-20 20:11:37 +0100

    refactor: introduce fixtures.ts for organizing test objects

---
+++ test/fixtures.ts
@@ -0,0 +1,33 @@
+import { UserDetails } from "../src/types/user-types";
+
+export const maggie: UserDetails = {
+  firstName: "Maggie",
+  lastName: "Simpson",
+  email: "maggie@simpson.com",
+  password: "secret",
+  role: "user",
+};
+
+export const testUsers = [
+  {
+    firstName: "Homer",
+    lastName: "Simpson",
+    email: "homer@simpson.com",
+    password: "secret",
+    role: "user",
+  },
+  {
+    firstName: "Marge",
+    lastName: "Simpson",
+    email: "marge@simpson.com",
+    password: "secret",
+    role: "user",
+  },
+  {
+    firstName: "Bart",
+    lastName: "Simpson",
+    email: "bart@simpson.com",
+    password: "secret",
+    role: "user",
+  },
+];
--- test/models/user-model-test.ts
+++ test/models/user-model-test.ts
@@ -1,17 +1,9 @@
 import { assert } from "chai";
 import { db } from "../../src/models/db";
-import { UserDetails } from "../../src/types/user-types";
 import { assertSubset } from "../test-utils";
+import { maggie } from "../fixtures";
 
 suite("User model tests", () => {
-  const maggie: UserDetails = {
-    firstName: "Maggie",
-    lastName: "Simpson",
-    email: "maggie@simpson.com",
-    password: "secret",
-    role: "user",
-  };
-
   setup(async () => {
     db.init("json");
   });
