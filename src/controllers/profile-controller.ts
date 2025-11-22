d2a7a76a0895b9cc6cd00a924afc2d575d7b2a0c
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-22 09:01:06 +0100

    feat: pass user details to view for updating profile

--- src/controllers/profile-controller.ts
+++ src/controllers/profile-controller.ts
@@ -1,9 +1,26 @@
 import { Request, ResponseToolkit } from "@hapi/hapi";
+import { db } from "../models/db";
 
 export const profileController = {
   index: {
     handler: function (request: Request, h: ResponseToolkit) {
-      return h.view("profile", { title: "Placemark Profile" });
+      const loggedInUser = request.auth.credentials;
+      const user = {
+        _id: loggedInUser._id,
+        userDetails: {
+          firstName: loggedInUser.firstName,
+          lastName: loggedInUser.lastName,
+          email: loggedInUser.email,
+          password: loggedInUser.password,
+        },
+      };
+      return h.view("profile", { title: "Placemark Profile", user: user });
+    },
+  },
+  updateUser: {
+    handler: async function (request: Request, h: ResponseToolkit) {
+      const { id } = request.params;
+      const user = await db.userStore?.getUserById(id);
     },
   },
 };
