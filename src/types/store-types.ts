1b57bcadf2b48055aec636d6cc10f200983a2b2b
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-23 06:28:33 +0100

    feat: add store type for poi-store

--- src/types/store-types.ts
+++ src/types/store-types.ts
@@ -1,3 +1,4 @@
+import { PointOfInterest, PointOfInterestDetails } from "./poi-types";
 import { User, UserDetails } from "./user-types";
 
 export interface UserStore {
@@ -8,6 +9,15 @@ export interface UserStore {
   updateUser(user: User, updatedUser: UserDetails): Promise<void>;
   deleteAllUsers(): Promise<void>;
   deleteUserById(id: string): Promise<void>;
+}
+
+export interface PointOfInterestStore {
+  addPOI(poi: PointOfInterestDetails): Promise<PointOfInterest>;
+  getAllPOIs(): Promise<PointOfInterest[]>;
+  getPOIById(id: string): Promise<PointOfInterest | null>;
+  updatePOI(poi: PointOfInterest, updatedPOI: PointOfInterestDetails): Promise<void>;
+  deleteAllPOIs(): Promise<void>;
+  deletePOIById(id: string): Promise<void>;
 }
 
 export interface jsonDb {
