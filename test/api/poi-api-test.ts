8aa0bd862230a569cc07f851674a8e4961a2e123
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-11-27 09:41:07 +0100

    test: add POI endpoint tests

---
+++ test/api/poi-api-test.ts
@@ -0,0 +1,98 @@
+import { assert } from "chai";
+import { db } from "../../src/models/db.js";
+import { service } from "./service.js";
+import { neuschwansteinCastle, testPOIs } from "../fixtures.js";
+import { PointOfInterestDetails } from "../../src/types/poi-types.js";
+
+const pois = new Array(testPOIs.length);
+
+suite("POI API tests", () => {
+  setup(async () => {
+    db.init("json");
+
+    await service.deleteAllPOIs();
+    for (let i = 0; i < testPOIs.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      pois[i] = await service.createPOI(testPOIs[i]);
+    }
+  });
+  teardown(async () => {});
+
+  test("create POI", async () => {
+    const newPOI = await service.createPOI(neuschwansteinCastle);
+    const newPOIDetails: PointOfInterestDetails = {
+      name: newPOI.name,
+      description: newPOI.description,
+      location: {
+        lat: newPOI.location.lat,
+        lng: newPOI.location.lng,
+      },
+    };
+    assert.deepEqual(newPOIDetails, neuschwansteinCastle);
+    assert.isDefined(newPOI._id);
+  });
+
+  test("get POI - success", async () => {
+    const returnedPOI = await service.getPOI(pois[0]._id);
+    assert.deepEqual(returnedPOI, pois[0]);
+  });
+
+  test("get POI - bad id", async () => {
+    try {
+      const returnedPOI = await service.getPOI("123");
+      assert.fail("Should not return a response");
+    } catch (error: any) {
+      assert(error.response.data.message === "No POI with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
+
+  test("get POI - deleted POI", async () => {
+    await service.deleteAllPOIs();
+
+    try {
+      const returnedPOI = await service.getPOI(pois[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error: any) {
+      assert(error.response.data.message === "No POI with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
+
+  test("update POI", async () => {
+    const updatedDetails: PointOfInterestDetails = {
+      name: "Neuschwanstein Castle Updated",
+      description: "Updated: A 19th-century Romanesque Revival palace built by King Ludwig II.",
+      location: {
+        lat: "48.0",
+        lng: "11.0",
+      },
+    };
+    await service.updatePOI(pois[0]._id, updatedDetails);
+    const updatedPOI = await service.getPOI(pois[0]._id);
+
+    assert.equal(updatedPOI.name, updatedDetails.name);
+    assert.equal(updatedPOI.description, updatedDetails.description);
+    assert.deepEqual(updatedPOI.location, updatedDetails.location);
+  });
+
+  test("delete all POIs", async () => {
+    let returnedPOIs = await service.getAllPOIs();
+    assert.equal(returnedPOIs.length, 3);
+    await service.deleteAllPOIs();
+    returnedPOIs = await service.getAllPOIs();
+    assert.equal(returnedPOIs.length, 0);
+  });
+
+  test("delete POI", async () => {
+    const poi = await service.createPOI(neuschwansteinCastle);
+    const response = await service.deletePOI(poi._id);
+    assert.equal(response.status, 204);
+    try {
+      const returnedPOI = await service.getPOI(poi._id);
+      assert.fail("Should not return a response");
+    } catch (error: any) {
+      assert(error.response.data.message === "No POI with this id", "Incorrect Response Message");
+    }
+  });
+});
