d267818dae95ce5ff2ad84768ef37dc53c4700ef
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-23 12:04:29 +0100

    feat: setup JWT utility functions

---
+++ src/api/jwt-utils.ts
@@ -0,0 +1,62 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request } from "@hapi/hapi";
+import dotenv from "dotenv";
+import { User } from "../types/user-types.js";
+import { db } from "../models/db.js";
+
+dotenv.config({ quiet: true });
+const cookiePassword = process.env.COOKIE_PASSWORD as string;
+
+export function createToken(user: User): string {
+  const payload = {
+    id: user._id,
+    email: user.email,
+    scope: [],
+  };
+
+  const options: jwt.SignOptions = {
+    algorithm: "HS256",
+    expiresIn: "1h",
+  };
+
+  return jwt.sign(payload, cookiePassword, options);
+}
+
+export function decodeToken(token: string): JwtPayload | null {
+  try {
+    const decoded = jwt.verify(token, cookiePassword) as jwt.JwtPayload;
+
+    return {
+      id: decoded.id,
+      email: decoded.email,
+      scope: decoded.scope,
+    } as JwtPayload;
+  } catch (err: any) {
+    console.error(err.message);
+  }
+  return null;
+}
+
+export async function validate(decoded: JwtPayload) {
+  const user = (await db.userStore?.getUserById(decoded.id)) as User;
+
+  if (user === null) {
+    return { isValid: false };
+  }
+  return { isValid: true, credentials: user };
+}
+
+export function getUserIdFromRequest(request: Request): string {
+  let userID = null;
+
+  try {
+    const { authorization } = request.headers;
+    const token = authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, cookiePassword) as jwt.JwtPayload;
+    userID = decodedToken.id;
+  } catch (err: any) {
+    userID = null;
+  }
+
+  return userID;
+}
--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,15 @@ import Inert from "@hapi/inert";
 import Handlebars from "handlebars";
 import Joi from "joi";
 import HapiSwagger from "hapi-swagger";
+import * as jwt from "hapi-auth-jwt2";
+
 import path from "path";
 import dotenv from "dotenv";
-
 import { fileURLToPath } from "url";
 import { apiRoutes, routes } from "./routes.js";
 import { db } from "./models/db.js";
 import { accountController } from "./controllers/account-controller.js";
+import { validate } from "./api/jwt-utils.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,6 +39,7 @@ async function init() {
   await server.register(Cookie);
   await server.register(Vision);
   await server.register(Inert);
+  await server.register(jwt);
   await server.register([
     Inert,
     Vision,
@@ -53,6 +56,11 @@ async function init() {
     },
     redirectTo: "/",
     validate: accountController.validate,
+  });
+  server.auth.strategy("jwt", "jwt", {
+    key: process.env.COOKIE_PASSWORD,
+    validate: validate,
+    verifyOptions: { algorithms: ["HS256"] },
   });
   server.auth.default({
     strategy: "session",
